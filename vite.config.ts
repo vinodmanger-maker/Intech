afc50e6b409bffa85c4de3278f72585d85621e6d
vinodmanger-maker <vinodmanger@gmail.com>
2026-01-18 22:05:37 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Intech/"
   plugins: [react()],
   base: './', // Ensures assets are loaded correctly on GitHub Pages (relative paths)
   build: {
